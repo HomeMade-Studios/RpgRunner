ece591dfc1737d65142f8804b2aac8d7ba96414f
Romboide82 <ugo98.juve@libero.it>
2015-03-11 17:59:58 +0100

    fws

---
+++ Assets/Scripts/Drops/ManaPotionDropping.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManaPotionDropping : MonoBehaviour {
+
+	//GameObject manaPotionButton;
+	float startTime;
+	// Use this for initialization
+	void Start () {
+		//manaPotionButton = GameObject.FindGameObjectWithTag ("ManaPotionButton");
+		transform.parent = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Transform>();
+		startTime = Time.time;
+		rigidbody2D.AddForce (new Vector2 (Random.Range (-5000, 5000), Random.Range (10000, 20000)));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTime >= 1.5) {
+			rigidbody2D.isKinematic = true;
+			//transform.position = Vector2.MoveTowards (transform.position, manaPotionButton.transform.position, 800 * Time.deltaTime);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "ManaPotionButton") {
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,31 +4,70 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	public float knowback;
-	public int maxHealth, strong;
+	public int minGoldDrop, maxGoldDrop;
+	public int maxHealth, strenght;
+	public float knockback;
+	public GameObject gold, healthPotion, manaPotion;
 	Character character;
-	int health;
+	bool dropHealthPotion, dropManaPotion;
+	int health, coinsDrop;
 	Slider healthBar;
 
 	// Use this for initialization
 	void Start () {
+		//gold = GameObject.FindGameObjectWithTag ("Gold");
+		//healthPotion = GameObject.FindGameObjectWithTag ("HealthPotion");
+		//manaPotion = GameObject.FindGameObjectWithTag ("ManaPotion");
 		character = GameObject.FindGameObjectWithTag ("Player").GetComponent<Character>();
 		healthBar = GetComponentInChildren <Slider>();
 		health = maxHealth;
+		DropSet ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		healthBar.value = (float)(health*1)/maxHealth;
 		if (health <= 0)
-			Destroy(gameObject);
+			Death ();
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
 		if(other.gameObject.tag == "Player"){
-			health -= 10;
-			character.attacked(strong);
-			rigidbody2D.AddForce (new Vector2(knowback,0));
+			character.Hit(strenght);
 		}
 	}
+
+	public void Hit (int strong){
+		rigidbody2D.AddForce (new Vector2(knockback,0));
+		health -= (int)(strong * Random.Range (0.5f, 1.5f));
+	}
+
+	void Death(){
+		Drop ();
+		Destroy (gameObject);
+	}
+
+	void Drop(){
+		for (int i = 0; i < coinsDrop; i++) {
+			Instantiate (gold, transform.position, transform.rotation);
+		}
+		if(dropHealthPotion)
+			Instantiate (healthPotion, transform.position, transform.rotation);
+		if(dropManaPotion)
+			Instantiate (manaPotion, transform.position, transform.rotation);
+	}
+
+	void DropSet(){
+		coinsDrop = Random.Range (minGoldDrop, maxGoldDrop);
+		if (Random.Range (1, 20) == 1)
+			dropHealthPotion = true;
+		else
+			dropHealthPotion = false;
+		if (Random.Range (1, 15) == 1)
+			dropManaPotion = true;
+		else
+			dropManaPotion = false;
+	}
+
+
 }
